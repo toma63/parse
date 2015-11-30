a09b5791946cc682afc666aba716ae86c9f234e8
toma <t_arns@yahoo.com>
2015-11-29 23:42:37 -0800

    MatchSyntax takes compiled regexps, added TakeUntilRE

--- parse.go
+++ parse.go
@@ -3,17 +3,16 @@ package parse
 import ("os"
         "bufio"
         "regexp"
-	"errors"
 )
 
 // line reader for concurrent use
 // filepath, line comment string, and an output chan containing lines
 // of text.  Comments and blank lines are stripped
 // Closes the channel on EOF
-func ReadLinesStripped(filepath string, line_com string, lines chan<- string) {
+func ReadLinesStripped(filepath string, line_comment string, lines chan<- string) {
 
         // match comment string to end of line
-        com_re := regexp.MustCompile(line_com + ".*$")
+        com_re := regexp.MustCompile(line_comment + ".*$")
 
         fd, err := os.Open(filepath)
 
@@ -74,28 +73,33 @@ func TakeUntil(stop string, ch <-chan string) []string {
 	}
 }
 
+// remove tokens from a string channel until stop regexp is seen
+func TakeUntilRE(stop_re *regexp.Regexp, ch <-chan string) []string {
+	res := []string{}
+	for {
+		tok, ok := <- ch
+		if ! ok { panic("parse error, premature end of file") } 
+		res = append(res, tok)
+		if stop_re.MatchString(tok) { return(res) }
+	}
+}
+
 // map a slice of regexps against a slice of strings
 // returning a boolean indicating whether all match
-func MatchSyntax(tokens []string, template []string) (bool, error) {
+func MatchSyntax(tokens []string, template []*regexp.Regexp) bool {
 
 	//check for length match
 	size := len(template)
 	if size != len(tokens) {
-		return false, errors.New("MatchSyntax: lengths don't match")
-	}
-
-	// compile the regexps in the template
-	re_templ := make([]*regexp.Regexp, len(template))
-	for i := 0 ; i < size ; i++ {
-		re_templ[i] = regexp.MustCompile(template[i])
+		return false
 	}
 
 	match := true
 
 	for i := 0 ; i < size ; i++ {
-		if ! re_templ[i].MatchString(tokens[i]) {
+		if ! template[i].MatchString(tokens[i]) {
 			match = false
 		}
 	}
-	return match, nil
+	return match
 }
