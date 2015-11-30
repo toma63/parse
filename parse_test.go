78240822aac193807a136a01e684985a8c54fc36
toma <t_arns@yahoo.com>
2015-11-29 23:43:31 -0800

    added test and test data for ReadLinesStripped

--- parse_test.go
+++ parse_test.go
@@ -1,28 +1,48 @@
 package parse
 
-import "testing"
+import ("testing"
+	"regexp"
+)
 
 func TestMatchSyntax(t *testing.T) {
-	template := []string{`\+`, `PLACED`, `\(`, `-?\d+`, `-?\d+`, `\)`, `FW`}
+	template_patt := []string{`\+`, `PLACED`, `\(`, `-?\d+`, `-?\d+`, `\)`, `FW`}
+	template := make([]*regexp.Regexp, len(template_patt))
+	for i := 0 ; i < len(template_patt) ; i++ {
+		template[i] = regexp.MustCompile(template_patt[i])
+	}
 	tokens := []string{"+", "PLACED", "(", "-1000", "1000", ")", "FW"}
 	bogus_tokens := []string{"+", "PLACED", "(", "-1000", "1000", ")", "FO"}
 	short := []string{"(", "-1000", "1000", ")", "FO"}
 
-	res, _ := MatchSyntax(tokens, template)
+	res := MatchSyntax(tokens, template)
 	if res != true {
 		t.Errorf("MatchSyntax(%q, %q) should have matched but did not", tokens, template)
 	}
 
-	res, _ = MatchSyntax(bogus_tokens, template)
+	res = MatchSyntax(bogus_tokens, template)
 	if res != false {
 		t.Errorf("MatchSyntax(%q, %q) should have matched but did not", bogus_tokens, template)
 	}
 
-	_, err := MatchSyntax(short, template)
-	if err == nil {
-		t.Errorf("MatchSyntax(%q, %q) should have errored on length mismatch", short, template)
+	res = MatchSyntax(short, template)
+	if res != false {
+		t.Errorf("MatchSyntax(%q, %q) should have failed on length mismatch", short, template)
 	}
-	if err.Error() != "MatchSyntax: lengths don't match" {
-		t.Error("Error should have been: 'MatchSyntax: lengths don't match")
+}
+
+func TestReadLinesStripped(t *testing.T) {
+	line_ch := make(chan string, 10)
+	ReadLinesStripped("./testdata/rltest", "//", line_ch)
+	lc := 0
+	lines := []string{"asd", "d", "dsasda ",
+		"dsa", "asd ad asdasdre33r", "sdf", "r89768w6er"}
+	for line := range line_ch {
+		if line != lines[lc] {
+			t.Errorf("ReadLinesStripped: line mismatch %q did not matc %q", line, lines[lc])
+		}
+		lc++
+	}
+	if lc != len(lines) {
+		t.Errorf("ReadLinesStripped: line count mismatch %q did not matc %q", lc, len(lines))
 	}
 }
